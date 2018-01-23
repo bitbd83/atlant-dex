7e178ffaecc485fa518e83aa716115c360f16c29
worreless <test5@etlgr.com>
2018-01-23 23:01:12 +0700

    Small refact dropdown component and modal window for fiat transactions

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -1,8 +1,8 @@
 <template lang="pug">
 .ddropdown
-  select.ddropdown__select(@change="select($event.target.value)")
+  select.ddropdown__select(@change="select($event.target.value)", :class="{'ddropdown__select--witchBorder': border}")
     option.ddropdown__option(v-for="option in options", :value="option") {{option}}
-  Icon.ddropdown__triangle(id="triangle2")
+  Icon.ddropdown__triangle(id="triangle2", :class="{'ddropdown__triangle--witchPadding': border}")
 </template>
 
 <script>
@@ -26,6 +26,11 @@ export default {
       default: () => [],
       required: true,
     },
+    border: {
+      type: Boolean,
+      default: false,
+      required: false,
+    },
   },
   components: {
     Icon,
@@ -46,6 +51,13 @@ export default {
     font-weight: bold;
     appearance: none;
     background-color: transparent;
+    &--witchBorder {
+      border: 1px solid #ffffff;
+      border-radius: 4px;
+      background: transparent;
+      padding: 10px;
+      font-size: 14px;
+    }
   }
   &__option {
     color: #000;
@@ -57,6 +69,9 @@ export default {
     position: absolute;
     right: 0;
     pointer-events: none;
+    &--witchPadding {
+      right: 10px;
+    }
   }
 }
 </style>
