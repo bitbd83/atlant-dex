93c8edf2c1193a96a32e04a6fe7b03b735708fbb
Volodymyr Antoniuk <va@atlant.io>
2018-03-26 23:58:56 +0300

    ATL-150 day end

---
+++ src/components/pages/Verification/VerificationForm.vue
@@ -0,0 +1,100 @@
+<template lang="pug">
+form.verification-form
+  verification-form-group(
+    label="Foo Name:",
+    validatable,
+    valid,
+  )
+    verification-input.verification-form__input
+  verification-form-group(
+    label="Last Name:",
+    validatable,
+  )
+    verification-input.verification-form__input(
+    )
+  verification-form-group(
+    label="Country:",
+    validatable,
+  )
+    verification-select.verification-form__input(
+    :options="['Foo', 'Bar']",
+    )
+  verification-form-group(
+    label="City:"
+    validatable,
+  )
+    verification-select.verification-form__input(
+    :options="['Foo', 'Bar', 'FooBar']",
+    )
+  verification-form-group(
+    label="Street Address:"
+    validatable,
+  )
+    verification-input.verification-form__input(
+    )
+  verification-form-group(
+    label="Postcode:"
+    validatable,
+  )
+    verification-input.verification-form__input(
+    )
+  verification-form-group(
+    label="Phone Number:"
+    validatable,
+  )
+    verification-input.verification-form__input(
+    )
+  verification-form-group(
+    label="Date of Birth:"
+    validatable,
+  )
+    verification-select.verification-form__input.verification-form__input--small(
+    placeholder="Day"
+    )
+    verification-select.verification-form__input.verification-form__input--small(
+    placeholder="Month"
+    )
+    verification-select.verification-form__input.verification-form__input--small(
+    placeholder="Year"
+    )
+  verification-form-group(
+    label="ID or Passport #:"
+    validatable,
+  )
+    verification-input.verification-form__input(
+    )
+</template>
+
+<script>
+import VerificationFormGroup from './VerificationFormGroup';
+import VerificationInput from './VerificationInput';
+import VerificationSelect from './VerificationSelect';
+
+export default {
+  name: 'VerificationDropdown',
+  components: {
+    VerificationSelect,
+    VerificationFormGroup,
+    VerificationInput,
+  },
+};
+</script>
+
+<style lang="scss" rel="stylesheet/scss" scoped>
+  .verification-form {
+    & &__input {
+      width: 174px;
+      &--small {
+        width: 84px;
+      }
+    }
+    &__input--small {
+      & + & {
+        margin-left: 25px;
+      }
+    }
+    /deep/ .verification-form-group__label-text {
+      width: 121px;
+    }
+  }
+</style>
---
+++ src/components/pages/Verification/VerificationInput.vue
@@ -0,0 +1,40 @@
+<template lang="pug">
+input.verification-input(
+  type="text",
+  :placeholder="placeholder",
+)
+</template>
+
+<script>
+export default {
+  name: 'VerificationInput',
+  props: {
+    placeholder: {
+      default: '-',
+      type: String,
+    },
+  },
+};
+</script>
+
+<style lang="scss" rel="stylesheet/scss" scoped>
+  @import "~sass/variables";
+  .verification-input {
+    background-color: transparent;
+    border-radius: 0;
+    border: $input-border;
+    padding: $input-padding;
+    transition: $input-transition;
+    width: 100%;
+    min-height: $input-height;
+
+    &::placeholder {
+      color: $input-placeholder-color;
+    }
+
+    &:hover,
+    &:focus {
+      border-color: $input-border-color-highlight;
+    }
+  }
+</style>
