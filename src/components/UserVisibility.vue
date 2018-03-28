ba61255a46a63d13d3844ed6d882b9d3802a152e
Volodymyr Antoniuk <va@atlant.io>
2018-03-28 10:46:37 +0300

    Add component

---
+++ src/components/UserVisibility.vue
@@ -0,0 +1,24 @@
+<script>
+import {mapGetters} from 'vuex';
+
+export default {
+  name: 'UserVisibility',
+  render() {
+    if (this.isHide) return null;
+    return this.$slots.default[0];
+  },
+  computed: {
+    ...mapGetters('membership', [
+        'isLoggedIn',
+    ]),
+    isHide() {
+      return (this.hideOnLogout && !this.isLoggedIn)
+      || (this.hideOnLogin && this.isLoggedIn);
+    },
+  },
+  props: {
+    hideOnLogout: Boolean,
+    hideOnLogin: Boolean,
+  },
+};
+</script>
