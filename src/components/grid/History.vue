d8b7173b6bca27d613e9a174caeafbfb3904164c
worreless <test5@etlgr.com>
2018-04-11 19:38:17 +0700

    refact signaller for trade history

--- src/components/grid/History.vue
+++ src/components/grid/History.vue
@@ -2,7 +2,7 @@
 table.history
   tbody.history__body
     tr.history__row(v-for='(trade, index) in lastTrades')
-      td.history__cell(:class="`history__cell--${(trade[3]) ? 'sell' : 'buy'}`") {{trade.price}}
+      td.history__cell(:class="`history__cell--${(trade.side) ? 'sell' : 'buy'}`") {{trade.price}}
       td.history__cell {{trade.amount.toFixed(4)}}
       td.history__cell {{(trade.price * trade.amount).toFixed(4)}}
 </template>
