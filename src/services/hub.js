6592ba8f1d0c0cd7bad6c1eb1f9b3ed6399c0e71
worreless <test5@etlgr.com>
2018-08-06 19:05:27 +0700

    Revert "fix: update signalr"
    This reverts commit 590dca615febfefa5f56ee25a147ec1f6ca7b199.

--- src/services/hub.js
+++ src/services/hub.js
@@ -2,13 +2,31 @@
 // Use of this source code is governed by Microsoft Reference Source
 // License (MS-RSL) that can be found in the LICENSE file.
 
-import {HubConnectionBuilder} from '@aspnet/signalr/dist/browser/signalr.min.js';
+// import {hubConnection} from 'signalr-no-jquery';
 
 export default {
   install(Vue, url, token, pair) {
-    let connection = new HubConnectionBuilder()
-      .withUrl(`${url}?token=${token}&pair=${pair}`)
-      .build();
+    const signalR = require('@aspnet/signalr');
+    let connection = new signalR.HubConnection(`${url}?token=${token}&pair=${pair}`);
     Vue.prototype.$hub = connection;
+    // {
+      // proxy: null,
+      // init() {
+      //   this.proxy = connection.createHubProxy(name);
+      //   connection.logging = false;
+      //   connection.disconnected(() => {
+      //     return this.start();
+      //   });
+      // },
+      // start() {
+      //   return new Promise((resolve, reject) => {
+      //     this.connection.start().then((status) => {
+      //       return resolve(status);
+      //     }).fail((error) => {
+      //       return reject(error);
+      //     });
+      //   });
+      // },
+    // };
   },
 };
