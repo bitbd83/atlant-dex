bd57f5542723f9a309cd05ee785027296529b417
worreless <test5@etlgr.com>
2018-08-06 20:08:03 +0700

    fix: signalr(ATL-566)

--- src/services/hub.js
+++ src/services/hub.js
@@ -2,31 +2,14 @@
 // Use of this source code is governed by Microsoft Reference Source
 // License (MS-RSL) that can be found in the LICENSE file.
 
-// import {hubConnection} from 'signalr-no-jquery';
+
+import {HubConnectionBuilder} from '@aspnet/signalr/dist/browser/signalr.min.js';
 
 export default {
   install(Vue, url, token, pair) {
-    const signalR = require('@aspnet/signalr');
-    let connection = new signalR.HubConnection(`${url}?token=${token}&pair=${pair}`);
+    let connection = new HubConnectionBuilder()
+      .withUrl(`${url}?token=${token}&pair=${pair}`)
+      .build();
     Vue.prototype.$hub = connection;
-    // {
-      // proxy: null,
-      // init() {
-      //   this.proxy = connection.createHubProxy(name);
-      //   connection.logging = false;
-      //   connection.disconnected(() => {
-      //     return this.start();
-      //   });
-      // },
-      // start() {
-      //   return new Promise((resolve, reject) => {
-      //     this.connection.start().then((status) => {
-      //       return resolve(status);
-      //     }).fail((error) => {
-      //       return reject(error);
-      //     });
-      //   });
-      // },
-    // };
   },
 };
