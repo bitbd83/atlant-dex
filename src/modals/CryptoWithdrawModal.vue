e4c95cb574c59b89b6e8b7e96b3d5b82bacf44d9
worreless <test5@etlgr.com>
2018-08-07 17:12:00 +0700

    fix: if in modal layout

--- src/modals/CryptoWithdrawModal.vue
+++ src/modals/CryptoWithdrawModal.vue
@@ -3,7 +3,7 @@
 // License (MS-RSL) that can be found in the LICENSE file.
 
 <template lang="pug">
-ModalLayout(:step="step", isWith2fa="true" :isSuccess="isSuccess", :title="`Withdraw ${data.currency}`")
+ModalLayout(:step="step", :isWith2fa="true" :isSuccess="isSuccess", :title="`Withdraw ${data.currency}`")
   .cryptoWithdraw
     .cryptoWithdraw__header
       .cryptoWithdraw__title Withdraw {{data.currency}}
