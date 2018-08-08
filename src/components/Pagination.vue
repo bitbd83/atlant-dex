ca65a2991567ce90d422611be074d670bdeeb567
worreless <test5@etlgr.com>
2018-08-08 22:22:49 +0700

    restyle: pagination

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -4,36 +4,36 @@
 
 <template lang="pug">
   .pagination(v-show="pageCount > 1")
-        .pagination__container
-          Icon.pagination__arrow.pagination__arrow-first(
-            id="icon-pagination-global",
-            :class="{'pagination__arrow-active' : page > 1}",
-            @click="page > 1 ? pageAction(1) : ''"
-          )
-          Icon.pagination__arrow.pagination__arrow-before(
-            id="icon-pagination",
-            :class="{'pagination__arrow-active' : page > 1}",
-            @click="page > 1 ? pageAction(page - 1) : ''"
-          )
-          .pagination__number(v-show="page > 3" , @click="pageAction(1)") 1
-          .pagination__space(v-show="page > 4") ...
-          .pagination__number(v-show="(page - 2) > 0", @click="pageAction(page - 2)") {{page - 2}}
-          .pagination__number(v-show="(page - 1) > 0", @click="pageAction(page - 1)") {{page - 1}}
-          .pagination__number.pagination__number-active Page {{page}}
-          .pagination__number(v-show="(page + 1) < pageCount", @click="pageAction(page+1)") {{page + 1}}
-          .pagination__number(v-show="(page + 2) < pageCount", @click="pageAction(page + 2)") {{page + 2}}
-          .pagination__space(v-show="(page + 3) < pageCount") ...
-          .pagination__number(v-show="page < pageCount", @click="pageAction(pageCount)") {{pageCount}}
-          Icon.pagination__arrow.pagination__arrow-next(
-            id="icon-pagination",
-            :class="{'pagination__arrow-active' : page < pageCount}",
-            @click="page < pageCount ? pageAction(page + 1) : ''"
-          )
-          Icon.pagination__arrow.pagination__arrow-last(
-            id="icon-pagination-global",
-            :class="{'pagination__arrow-active' : page < pageCount}",
-            @click="page < pageCount ? pageAction(pageCount) : '' "
-          )
+    .pagination__container
+      Icon.pagination__arrow.pagination__arrow-first(
+        id="icon-pagination-global",
+        :class="{'pagination__arrow-active' : page > 1}",
+        @click="page > 1 ? pageAction(1) : ''"
+      )
+      Icon.pagination__arrow.pagination__arrow-before(
+        id="icon-pagination",
+        :class="{'pagination__arrow-active' : page > 1}",
+        @click="page > 1 ? pageAction(page - 1) : ''"
+      )
+      .pagination__number(v-show="page > 3" , @click="pageAction(1)") 1
+      .pagination__space(v-show="page > 4") ...
+      .pagination__number(v-show="(page - 2) > 0", @click="pageAction(page - 2)") {{page - 2}}
+      .pagination__number(v-show="(page - 1) > 0", @click="pageAction(page - 1)") {{page - 1}}
+      .pagination__number.pagination__number-active {{page}} page
+      .pagination__number(v-show="(page + 1) < pageCount", @click="pageAction(page+1)") {{page + 1}}
+      .pagination__number(v-show="(page + 2) < pageCount", @click="pageAction(page + 2)") {{page + 2}}
+      .pagination__space(v-show="(page + 3) < pageCount") ...
+      .pagination__number(v-show="page < pageCount", @click="pageAction(pageCount)") {{pageCount}}
+      Icon.pagination__arrow.pagination__arrow-next(
+        id="icon-pagination",
+        :class="{'pagination__arrow-active' : page < pageCount}",
+        @click="page < pageCount ? pageAction(page + 1) : ''"
+      )
+      Icon.pagination__arrow.pagination__arrow-last(
+        id="icon-pagination-global",
+        :class="{'pagination__arrow-active' : page < pageCount}",
+        @click="page < pageCount ? pageAction(pageCount) : '' "
+      )
 </template>
 
 <script>
@@ -54,42 +54,47 @@ export default {
 </script>
 
 <style lang="scss">
+@import "variables";
+
 .pagination {
   display: flex;
-  justify-content: center;
   margin: 46px 0;
 
   &__container {
     display: flex;
-    justify-content: center;
+    justify-content: flex-start;
     align-items: center;
   }
 
   &__number {
-    color: #ffffff;
+    color: $color__black;
     font-size: 16px;
     font-weight: 400;
-    margin: 0 8px;
+    margin: 0 15px;
     white-space: nowrap;
     cursor: pointer;
     &-active {
-      color: #ffc600;
+      margin: 0 30px;
+      color: $color__blue;
       font-weight: 700;
       cursor: default;
     }
   }
 
   &__arrow {
+    width: 14px;
     height: 8px;
-    fill: #ffffff;
-    opacity: 0.5;
+    fill: $background__dark;
+    opacity: .5;
     margin: 0 18.5px;
     &-active {
+      fill: $background__blue;
       opacity: 1;
       cursor: pointer;
     }
     &-first {
       transform: rotate(180deg);
+      margin-left: 4px;
     }
     &-before {
       transform: rotate(180deg);
