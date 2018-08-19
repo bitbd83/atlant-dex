021deef2fe6fa2e2f39a5190a7c0499003799516
xcip <40529853+xcip@users.noreply.github.com>
2018-08-19 04:40:16 +0300

    travis: suppress auth checks in deploy.sh

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,6 +4,11 @@ eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 .travis/deploy-key # Allow read access to the private key
 ssh-add .travis/deploy-key # Add the private key to SSH
 
+# pass host authenticity check
+echo -e "Host $IP\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+ssh-keyscan $IP >> ~/.ssh/known_hosts
+
+# push changes to the deployment environment
 git config --global push.default matching
 git remote add deploy ssh://deploy@$IP$DEPLOY_DIR
 git push deploy dev
