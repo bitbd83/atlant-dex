c7b044c15f707ddba12f706750c94c0421cf08ea
worreless <test5@etlgr.com>
2018-08-28 22:28:45 +0700

    feat: add widget extended info

--- src/components/grid/ExtendedInfo.vue
+++ src/components/grid/ExtendedInfo.vue
@@ -16,7 +16,24 @@ Tile(
     :height="data.height"
     :width="data.width"
     :name="data.name"
-  ) Extended Info
+  )
+    .extendedInfo
+      .extendedInfo__title MAIN FACTS
+      .extendedInfo__container(v-scrollbar="")
+        ul.extendedInfo__list
+          li.extendedInfo__item Condo
+          li.extendedInfo__item Built in 2003
+          li.extendedInfo__item Days on Atlant: Less than 1 day on
+          li.extendedInfo__item Atlant
+          li.extendedInfo__item Views since listing: 25
+          li.extendedInfo__item 0 shoppers saved this home
+        ul.extendedInfo__list
+          li.extendedInfo__item Pets: Contact manager
+          li.extendedInfo__item Parking: 1 space
+          li.extendedInfo__item Price/sqft: $980
+          li.extendedInfo__item Built by: LandSea Homes
+          li.extendedInfo__item View Interactive Floorplan
+          li.extendedInfo__item Community name: Avora
 </template>
 
 <script>
@@ -40,4 +57,44 @@ export default {
 
 <style lang="scss">
 @import 'variables';
+.extendedInfo {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  background-color: $background__white;
+  padding: 10px 0 10px 10px;
+  border-radius: 8px;
+  border: 1px solid $color__grey_border;
+  padding: 28px 0 31px 0;
+
+  &__title {
+    font-family: "Century Gothic";
+    font-size: 14px;
+    font-weight: 700;
+    line-height: 35px;
+    text-transform: uppercase;
+    margin-left: 65px;
+    margin-bottom: 8px;
+  }
+
+  &__container {
+    display: flex;
+    flex-grow: 2;
+  }
+
+  &__list {
+    flex-grow: 2;
+    margin-left: 78px;
+    list-style: square outside url('~assets/images/icon-list-point.png');
+  }
+
+  &__item {
+    font-family: "Century Gothic";
+    font-size: 14px;
+    line-height: 35px;
+    white-space: nowrap;
+    padding-left: 32px;
+  }
+}
 </style>
