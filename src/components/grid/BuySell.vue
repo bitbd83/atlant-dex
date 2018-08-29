f2d38a433d69972ef8bad40e3ffe1b7d80e4c2e5
h20xerox <h20xerox@gmail.com>
2018-08-29 16:01:31 +0700

    fix loader prop

--- src/components/grid/BuySell.vue
+++ src/components/grid/BuySell.vue
@@ -4,7 +4,7 @@
 
 <template lang='pug'>
 .buySell
-  Loader(isWhite="", :isLoading="loading")
+  Loader(isWidthOverflow="", isWhite="", :isLoading="loading")
   .buySell__headerContainer
     .buySell__header
       .title.buySell__title  {{baseCurrency}} / {{quoteCurrency}}
