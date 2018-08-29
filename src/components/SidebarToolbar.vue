8a55181f48999754c4087308377132eeb15e3d5f
gcofficial <gurievcreative@gmail.com>
2018-08-29 12:36:27 +0300

    feat( dark_theme ): SidebarToolbar

--- src/components/SidebarToolbar.vue
+++ src/components/SidebarToolbar.vue
@@ -126,6 +126,24 @@ export default {
   &:before{
     background-color: $background__dark_toolbar;
   }
+
+  &__iconWrap {
+
+    &--active {
+      background: $fill__dark_hover_toolbar;
+    }
+  }
+
+  &__icon {
+
+    &--active {
+      fill: $fill__white;
+
+      &:hover {
+        fill: $fill__white;
+      }
+    }
+  }
 }
 .toolbar {
   position: relative;
