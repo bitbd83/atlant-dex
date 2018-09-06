8cc87d4d2672fae999d587928cc63f7f23d049cf
xeroxh20 <xeroxh20@gmail.com>
2018-09-06 16:05:29 +0700

    refact: clean up alerts leftovers in vuex

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import createPersistedState from 'vuex-persistedstate';
-import alerts from './modules/alerts';
 import misc from './modules/misc';
 import localization from './modules/localization';
 import tradeInfo from './modules/tradeInfo';
@@ -24,7 +23,6 @@ Vue.use(Vuex);
 
 const store = new Vuex.Store({
   modules: {
-    alerts,
     misc,
     localization,
     tradeInfo,
